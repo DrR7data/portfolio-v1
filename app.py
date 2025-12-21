a8c52831d806482a25b27a328685c462ea577c13
DrR7data <robertorm29@gmail.com>
2025-12-21 04:09:12 +0000

    Edit app

--- app.py
+++ app.py
@@ -1,8 +1,31 @@
 from flask import Flask
+from flask import jsonify
 from flask import render_template
 from random import choices
 
 app = Flask(__name__)
+
+def change(amount):
+    # calculate the resultant change and store the result (res)
+    res = []
+    coins = [1,5,10,25] # value of pennies, nickels, dimes, quarters
+    coin_lookup = {25: "quarters", 10: "dimes", 5: "nickels", 1: "pennies"}
+
+    # divide the amount*100 (the amount in cents) by a coin value
+    # record the number of coins that evenly divide and the remainder
+    coin = coins.pop()
+    num, rem  = divmod(int(amount*100), coin)
+    # append the coin type and number of coins that had no remainder
+    res.append({num:coin_lookup[coin]})
+
+    # while there is still some remainder, continue adding coins to the result
+    while rem > 0:
+        coin = coins.pop()
+        num, rem = divmod(rem, coin)
+        if num:
+            if coin in coin_lookup:
+                res.append({num:coin_lookup[coin]})
+    return res
 
 
 def random_frase():
@@ -49,6 +72,23 @@ def frase():
 
     my_frase = random_frase()
     return render_template("index.html", title="Random frases", frase=my_frase)
+@app.route('/change/<dollar>/<cents>')
+def changeroute(dollar, cents):
+    print(f"Make Change for {dollar}.{cents}")
+    amount = f"{dollar}.{cents}"
+    result = change(float(amount))
+    return jsonify(result)
+    
+    
+@app.route('/100/change/<dollar>/<cents>')
+def change100route(dollar, cents):
+    print(f"Make Change for {dollar}.{cents}")
+    amount = f"{dollar}.{cents}"
+    amount100 = float(amount) * 100
+    print(f"This is the {amount} X 100")
+    result = change(amount100)
+    return jsonify(result)
+
 
 
 if __name__ == "__main__":
